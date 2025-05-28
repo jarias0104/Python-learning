56ce54c3b414be5cba24ee0013a4e4d99302bfb5
Jeshua Arias <ariasjeshua2@gmail.com>
2025-05-28 09:27:06 -0600

    Add files via upload

---
+++ 99_bottles.py
@@ -0,0 +1,23 @@
+#Jeshua Nauel Arias Santos
+# So in this program I have to print in the console the 99 Bottles of Beer lyrics using string interpolation with for loop and a range
+
+
+#str1 = "99 bottles of beer on the wall"
+#str2 = "99 bottles of beer"
+#str3 = "Take one down, pass it around"
+#str4 = "98 bottles of beer on the wall" 
+
+
+#str1 = "99 bottles of beer on the wall" , "99 bottles of beer", "Take one down, pass it around", "98 bottles on the wall"
+lyrics = ["99 bottles of beer on the wall",
+          "99 bottles of beer",
+          "Take one down, pass it around",
+          "98 bottles on the wall"]
+
+for i in range(1):  
+    for line in lyrics:
+        print(line)
+    print()
+
+ 
+
